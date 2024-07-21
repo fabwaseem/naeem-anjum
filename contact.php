bc8cfd17fc429fdcb3432dea61b70b1871e950ec
Waseem Anjum <waseemaofficial@gmail.com>
2024-07-21 20:11:12 +0500

    added layout variables

--- contact.php
+++ contact.php
@@ -1,51 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, maximum-scale=1"
-    />
-    <title>Naeem Anjum • Freelance Designer & Developer</title>
-    <meta name="description" content="" />
-    <meta
-      property="og:title"
-      content="Naeem Anjum • Freelance Designer & Developer"
-    />
-    <meta property="og:description" content="" />
-    <meta property="og:image" content="#" />
-    <meta property="og:site_name" content="Naeem Anjum" />
-    <meta property="og:locale" content="en" />
-    <meta property="og:type" content="website" />
-    <meta
-      property="twitter:title"
-      content="Naeem Anjum • Freelance Designer & Developer"
-    />
-    <meta property="twitter:description" content="" />
-    <meta property="twitter:image" content="#" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="google" content="notranslate" />
-    <meta property="og:locale" content="en" />
-    <link rel="canonical" href="/" />
 
-    <link
-      rel="preload"
-      as="font"
-      href="./assets/fonts/NeueMontreal-Regular.otf"
-      type="font/otf"
-      crossorigin="anonymous"
-    />
 
-    <link href="./assets/css/normalize.css" rel="stylesheet" />
-    <link href="./assets/css/locomotive-scroll.css" rel="stylesheet" />
-    <link href="./assets/css/styleguide.css" rel="stylesheet" />
-    <link href="./assets/css/components.css" rel="stylesheet" />
-    <link href="./assets/css/style-new.css" rel="stylesheet" />
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-  </head>
-
-  <body data-barba="wrapper">
-    <div class="no-scroll-overlay"></div>
+<?php include_once("./layout/header.php");
+ ?>
     <div class="loading-container">
       <div class="loading-screen" style="top: 100%">
         <div class="rounded-div-wrap top" style="height: 0vh">
@@ -179,232 +135,13 @@
       data-barba="container"
       data-barba-namespace="contact"
     >
-      <div class="btn btn-hamburger">
-        <div
-          class="btn-click magnetic"
-          data-strength="50"
-          data-strength-text="25"
-        >
-          <div class="btn-fill"></div>
-          <div class="btn-text">
-            <div class="btn-bars"></div>
-            <span class="btn-text-inner">Menu</span>
-          </div>
-        </div>
-      </div>
-      <div class="overlay fixed-nav-back"></div>
-      <div class="fixed-nav theme-dark">
-        <div class="fixed-nav-rounded-div">
-          <div class="rounded-div-wrap">
-            <div class="rounded-div"></div>
-          </div>
-        </div>
-        <div class="fixed-nav-inner">
-          <div class="row nav-row">
-            <h5>Navigation</h5>
-            <div class="stripe"></div>
-            <ul class="links-wrap">
-              <li class="btn btn-link">
-                <a
-                  href="/"
-                  class="btn-click magnetic"
-                  data-strength="24"
-                  data-strength-text="12"
-                >
-                  <span class="btn-text">
-                    <span class="btn-text-inner">Home</span>
-                  </span>
-                </a>
-              </li>
-              <li class="btn btn-link">
-                <a
-                  href="./work"
-                  class="btn-click magnetic"
-                  data-strength="24"
-                  data-strength-text="12"
-                >
-                  <span class="btn-text">
-                    <span class="btn-text-inner">Work</span>
-                  </span>
-                </a>
-              </li>
-              <li class="btn btn-link">
-                <a
-                  href="./about"
-                  class="btn-click magnetic"
-                  data-strength="24"
-                  data-strength-text="12"
-                >
-                  <span class="btn-text">
-                    <span class="btn-text-inner">About</span>
-                  </span>
-                </a>
-              </li>
-              <li class="btn btn-link active">
-                <a
-                  href="./contact"
-                  class="btn-click magnetic"
-                  data-strength="24"
-                  data-strength-text="12"
-                >
-                  <span class="btn-text">
-                    <span class="btn-text-inner">Contact</span>
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="row social-row">
-            <div class="stripe"></div>
-            <div class="socials">
-              <h5>Socials</h5>
-              <ul>
-                <li class="btn btn-link btn-link-external">
-                  <a
-                    href="https://www.behance.net/thisnaeem"
-                    target="_blank"
-                    class="btn-click magnetic"
-                    data-strength="20"
-                    data-strength-text="10"
-                  >
-                    <span class="btn-text">
-                      <span class="btn-text-inner">Behance</span>
-                    </span>
-                  </a>
-                </li>
-                <li class="btn btn-link btn-link-external">
-                  <a
-                    href="https://www.dribble.com/thisnaeem"
-                    target="_blank"
-                    class="btn-click magnetic"
-                    data-strength="20"
-                    data-strength-text="10"
-                  >
-                    <span class="btn-text">
-                      <span class="btn-text-inner">Dribble</span>
-                    </span>
-                  </a>
-                </li>
-                <li class="btn btn-link btn-link-external">
-                  <a
-                    href="https://www.instagram.com/thisnaeem/"
-                    target="_blank"
-                    class="btn-click magnetic"
-                    data-strength="20"
-                    data-strength-text="10"
-                  >
-                    <span class="btn-text">
-                      <span class="btn-text-inner">Instagram</span>
-                    </span>
-                  </a>
-                </li>
-                <li class="btn btn-link btn-link-external">
-                  <a
-                    href="https://twitter.com/thisnaeem"
-                    target="_blank"
-                    class="btn-click magnetic"
-                    data-strength="20"
-                    data-strength-text="10"
-                  >
-                    <span class="btn-text">
-                      <span class="btn-text-inner">Twitter</span>
-                    </span>
-                  </a>
-                </li>
-                <li class="btn btn-link btn-link-external">
-                  <a
-                    href="https://www.linkedin.com/in/thisnaeem/"
-                    target="_blank"
-                    class="btn-click magnetic"
-                    data-strength="20"
-                    data-strength-text="10"
-                  >
-                    <span class="btn-text">
-                      <span class="btn-text-inner">LinkedIn</span>
-                    </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+       <?php include_once("./layout/mobile-nav.php"); ?>
       <div class="main-wrap" data-scroll-container>
         <header
           class="section default-header contact-header theme-dark"
           data-scroll-section
         >
-          <div class="nav-bar">
-            <div class="credits-top">
-              <div class="btn btn-link btn-left-top">
-                <a
-                  href="/"
-                  class="btn-click magnetic"
-                  data-strength="20"
-                  data-strength-text="10"
-                >
-                  <span class="btn-text">
-                    <div class="credit"><span>©</span></div>
-                    <div class="cbd">
-                      <span class="dennis"
-                        ><span class="dennis-span">Naeem</span>
-                        <span class="snellenberg"> Anjum</span></span
-                      >
-                    </div>
-                  </span>
-                </a>
-              </div>
-            </div>
-            <ul class="links-wrap">
-              <li class="btn btn-link">
-                <a
-                  href="./work"
-                  class="btn-click magnetic"
-                  data-strength="20"
-                  data-strength-text="10"
-                >
-                  <span class="btn-text">
-                    <span class="btn-text-inner">Work</span>
-                  </span>
-                </a>
-              </li>
-              <li class="btn btn-link">
-                <a
-                  href="./about"
-                  class="btn-click magnetic"
-                  data-strength="20"
-                  data-strength-text="10"
-                >
-                  <span class="btn-text">
-                    <span class="btn-text-inner">About</span>
-                  </span>
-                </a>
-              </li>
-              <li class="btn btn-link active">
-                <a
-                  href="./contact"
-                  class="btn-click magnetic"
-                  data-strength="20"
-                  data-strength-text="10"
-                >
-                  <span class="btn-text">
-                    <span class="btn-text-inner">Contact</span>
-                  </span>
-                </a>
-              </li>
-              <li class="btn btn-link btn-menu">
-                <div
-                  class="btn-click magnetic"
-                  data-strength="20"
-                  data-strength-text="10"
-                >
-                  <div class="btn-text">
-                    <span class="btn-text-inner">Menu</span>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
+       <?php include_once("./layout/navbar.php"); ?>
           <div class="container medium">
             <div class="row once-in">
               <div class="flex-col">
@@ -461,7 +198,6 @@
                 <form
                   class="form"
                   method="post"
-                  action="./contact"
                   enctype="multipart/form-data"
                   novalidate
                 >
@@ -683,80 +419,93 @@
           class="section footer footer-contact theme-dark"
           data-scroll-section
         >
-          <div class="container no-padding">
-            <div class="row bottom-footer">
-              <div class="flex-col">
-                <div class="credits">
-                  <h5>Version</h5>
-                  <p>2024 © Edition</p>
+           <div class="container no-padding">
+              <div class="row bottom-footer">
+                <div class="flex-col">
+                  <div class="credits">
+                    <h5>Version</h5>
+                    <p>2024 © Edition</p>
+                  </div>
+                  <div class="time">
+                    <h5>Local time</h5>
+                    <p><span id="timeSpan">09:41 PM CET</span></p>
+                  </div>
                 </div>
-                <div class="time">
-                  <h5>Local time</h5>
-                  <p><span id="timeSpan">09:41 PM CET</span></p>
-                </div>
-              </div>
-              <div class="flex-col">
-                <div class="socials">
-                  <h5>Socials</h5>
-                  <ul>
-                    <li class="btn btn-link btn-link-external">
-                      <a
-                        href="https://www.awwwards.com/dennissnellenberg/"
-                        target="_blank"
-                        class="btn-click magnetic"
-                        data-strength="20"
-                        data-strength-text="10"
-                      >
-                        <span class="btn-text">
-                          <span class="btn-text-inner">Awwwards</span>
-                        </span>
-                      </a>
-                    </li>
-                    <li class="btn btn-link btn-link-external">
-                      <a
-                        href="https://www.instagram.com/codebydennis/"
-                        target="_blank"
-                        class="btn-click magnetic"
-                        data-strength="20"
-                        data-strength-text="10"
-                      >
-                        <span class="btn-text">
-                          <span class="btn-text-inner">Instagram</span>
-                        </span>
-                      </a>
-                    </li>
-                    <li class="btn btn-link btn-link-external">
-                      <a
-                        href="https://twitter.com/codebydennis"
-                        target="_blank"
-                        class="btn-click magnetic"
-                        data-strength="20"
-                        data-strength-text="10"
-                      >
-                        <span class="btn-text">
-                          <span class="btn-text-inner">Twitter</span>
-                        </span>
-                      </a>
-                    </li>
-                    <li class="btn btn-link btn-link-external">
-                      <a
-                        href="https://www.linkedin.com/in/dennissnellenberg/"
-                        target="_blank"
-                        class="btn-click magnetic"
-                        data-strength="20"
-                        data-strength-text="10"
-                      >
-                        <span class="btn-text">
-                          <span class="btn-text-inner">LinkedIn</span>
-                        </span>
-                      </a>
-                    </li>
-                  </ul>
-                  <div class="stripe"></div>
+                <div class="flex-col">
+                  <div class="socials">
+                    <h5>Socials</h5>
+                    <ul>
+                      <li class="btn btn-link btn-link-external">
+                        <a
+                          href="https://www.behance.net/thisnaeem"
+                          target="_blank"
+                          class="btn-click magnetic"
+                          data-strength="20"
+                          data-strength-text="10"
+                        >
+                          <span class="btn-text">
+                            <span class="btn-text-inner">Behance</span>
+                          </span>
+                        </a>
+                      </li>
+                      <li class="btn btn-link btn-link-external">
+                        <a
+                          href="https://www.dribble.com/thisnaeem"
+                          target="_blank"
+                          class="btn-click magnetic"
+                          data-strength="20"
+                          data-strength-text="10"
+                        >
+                          <span class="btn-text">
+                            <span class="btn-text-inner">Dribble</span>
+                          </span>
+                        </a>
+                      </li>
+                      <li class="btn btn-link btn-link-external">
+                        <a
+                          href="https://www.instagram.com/thisnaeem/"
+                          target="_blank"
+                          class="btn-click magnetic"
+                          data-strength="20"
+                          data-strength-text="10"
+                        >
+                          <span class="btn-text">
+                            <span class="btn-text-inner">Instagram</span>
+                          </span>
+                        </a>
+                      </li>
+                      <li class="btn btn-link btn-link-external">
+                        <a
+                          href="https://twitter.com/thisnaeem"
+                          target="_blank"
+                          class="btn-click magnetic"
+                          data-strength="20"
+                          data-strength-text="10"
+                        >
+                          <span class="btn-text">
+                            <span class="btn-text-inner">Twitter</span>
+                          </span>
+                        </a>
+                      </li>
+                      <li class="btn btn-link btn-link-external">
+                        <a
+                          href="https://www.linkedin.com/in/thisnaeem/"
+                          target="_blank"
+                          class="btn-click magnetic"
+                          data-strength="20"
+                          data-strength-text="10"
+                        >
+                          <span class="btn-text">
+                            <span class="btn-text-inner">LinkedIn</span>
+                          </span>
+                        </a>
+                      </li>
+                    </ul>
+                    <div class="stripe"></div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
         </footer>
       </div>
     </main>
